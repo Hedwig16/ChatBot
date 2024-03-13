353a5bad4797b00fb3f6729b60bfb8ac2fda652e
Hedwig16 <srivishnu91500@gmail.com>
2024-03-13 15:11:17 +0530

    FC

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      {/* Dialogflow Messenger integration */}
+      <df-messenger
+        project-id="darwinbox-412012"
+        agent-id="043b82b9-8396-44cf-8ceb-24638011ca13"
+        language-code="en"
+        max-query-length="-1"
+        allow-feedback="all">
+        <df-messenger-chat-bubble chat-title="darwinbot"></df-messenger-chat-bubble>
+      </df-messenger>
+    </div>
+  );
+}
+ 
+export default App;
